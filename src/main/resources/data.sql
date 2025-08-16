c3b7162cb2ff5c1e6602ed4b7703af971bec7fa5
Mattia Marini <marmattia26@gmail.com>
2025-08-16 14:29:57 +0200

    Added pwd crypt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO USERS (username, password, role)
-VALUES ('admin', '1234', 'ADMIN'),
-       ('user1', 'u1', 'USER');
+VALUES ('admin', '$2a$12$/x6QXEB1/jqBMBJgIDql8.hdbX2agK9KFXf05tU1r7ZTWZrMbHi7m', 'ADMIN'), -- pwd=1234
+       ('user1', '$2a$12$/1c5qJPx2l2PPuhE.4eGXeDhP6HamxGUogt3.rxK4dIlc09bKQLpe', 'USER');  --pwd=u1
