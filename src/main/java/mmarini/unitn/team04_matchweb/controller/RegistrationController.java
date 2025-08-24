378acdf50c3a250a750b2b9fd4e5f4e831ecedf8
Mattia Marini <marmattia26@gmail.com>
2025-08-24 20:59:52 +0200

    Fixed frontend and backend checks in registration page

--- src/main/java/mmarini/unitn/team04_matchweb/controller/RegistrationController.java
+++ src/main/java/mmarini/unitn/team04_matchweb/controller/RegistrationController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -28,8 +28,13 @@ public class RegistrationController {
 
     // Api
     @PostMapping("/api/signup")
-    public String registerUser(@ModelAttribute("registrationForm") RegistrationForm form, Model model) {
-        registrationService.register(form, "USER");
+    public String registerUser(@ModelAttribute("registrationForm") RegistrationForm form, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            registrationService.register(form, "USER");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+            return "redirect:/signup";
+        }
 
         return "redirect:/registration-successful";
     }
